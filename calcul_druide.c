751930468ccf81116c5e4803ef4e668609d326d9
votre-nom <dabovalkilian@gmail.com>
2025-11-04 09:47:58 +0100

    Premier commit du projet magique

---
+++ calcul_druide.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    printf("Bienvenu druide master !!\n");
+    printf("Entrer des chiffres suivis d'opérateur (il faut impérativement que le nombre d'opérateur soit égale au nombre de chiffre -1) : \n");
+    
+    return 0;
+}
